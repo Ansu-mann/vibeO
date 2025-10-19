f3e8312f1268d13b1ca4effd3bf1fd5fe3e344d2
ANSUMAN PANDA <124277891+Ansu-mann@users.noreply.github.com>
2025-10-19 09:23:57 +0530

    userProfile, delete video

--- database/db.js
+++ database/db.js
@@ -7,8 +7,12 @@ const connectToDb = async () => {
         }
         
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // // serverSelectionTimeoutMS: 30000, // 30 seconds
+            // // socketTimeoutMS: 45000, // 45 seconds
+            // // bufferMaxEntries: 0,
+            // // maxPoolSize: 10
         });
         
         console.log(`MongoDB is connected successfully!`);
---
+++ models/UserProfile.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const User = require('./User')
+
+const UserProfileSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    gender: {
+        type: String,
+        lowercase: true,
+        enum: ['male', 'female', "couldn't decide"]
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User
+    },
+    userName: {
+        type: mongoose.Schema.Types.String,
+        ref: User
+    }
+})
+
+module.exports = mongoose.model('UserProfile', UserProfileSchema)
